736afd87b0db5127e3693010e9c9c267b70a762a
Pooja B Shankar <nag.shankar6@gmail.com>
2020-05-03 14:43:53 +0530

    amount and createdAt formatted

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,17 @@ import React from "react";
 //import { connect } from 'react-redux';
 //import { removeExpense } from './../actions/expenses'
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import '../locales/inr.js';
 
 //now destructuring instead od taking props
 const ExpenseListItem = ({id, description, amount, createdAt}) => {
+    numeral.locale('inr');
   return(
     <div>
           <Link to={`/edit/${id}`}><h3>{description} </h3></Link>
-          <h4>Amount: {amount} Created: {createdAt}</h4>
+          <h4>Amount: {numeral(amount).format('$0,0.00')} Created: {moment(createdAt).format('MMMM Do, YYYY')}</h4>
          
           {/* lets have remove button only when wants to edit an expense*/} 
          
---
+++ src/locales/inr.js
@@ -0,0 +1,17 @@
+import numeral from 'numeral';
+
+numeral.register('locale', 'inr', {
+  delimiters: {
+    thousands: ',',
+    decimal: '.'
+  },
+  abbreviations: {
+    thousand: 'k',
+    million: 'm',
+    billion: 'b',
+    trillion: 't'
+  },
+  currency: {
+    symbol: '₹'
+  }
+});
